a07ec85d9dc93236d1a36d602a9e5c732fd1ac08
krasnykid <tmisiak10m@mat.umk.pl>
2024-06-13 23:29:06 +0200

    dodano decoding

--- tekst/main.py
+++ tekst/main.py
@@ -5,9 +5,7 @@ import os
 from collections import Counter
 
 from parser import args
-from kmp import replacePattern
-from huffman import HuffmanTree
-from nodetree import NodeTree
+from huffman import huffmanTree, encode, decode
 
 def main():
     if not os.path.exists("out"):
@@ -15,74 +13,28 @@ def main():
 
     infile = args.infile
     outfile = args.outfile
-    json_outfile = args.json
+    json = args.json
 
     if not os.path.exists(args.infile):
         print("Error: specified input file does not exist.")
         return
 
-    string = ""
-    string_fixed = ""
-    with open(infile) as f:
-        string = f.read()
-        string_fixed = replacePattern(string, 'poli', 'boli')
-        string_fixed = replacePattern(string_fixed, '\n', ' ').strip()
-
-    freq = Counter(string_fixed)
-    freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
-    print(freq)
-    nodes = freq
-
-    while len(nodes) > 1:
-        (key1, c1) = nodes[-1]
-        (key2, c2) = nodes[-2]
-        nodes = nodes[:-2]
-        node = NodeTree(key1, key2)
-        nodes.append((node, c1 + c2))
-        nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
-
-    huffman_code = HuffmanTree(nodes[0][0])
-
-    out = ''.join(f"{huffman_code[char]}" for char in string_fixed)
-    out_separated = ' '.join(f"{huffman_code[char]}" for char in string_fixed)
-
     if args.verbose:
-        print("ANALIZA PLIKU \"" + infile + "\" (" + str(len(string_fixed)) + " ZNAKÓW):")
-        print()
+        verbose = True
 
-        print(' Znak | Odpowiednik w sensie Huffmana ')
-        print('-' * 38)
-        for (char, frequency) in freq:
-            print(' %-4r |%20s' % (char, huffman_code[char]))
-        print()
-
-        print("ZAWARTOŚĆ \"" + infile + "\":")
-        print(string.strip())
-        print()
-        print("PLIK Z PODMIENIONĄ MELODIĄ:")
-        print(string_fixed)
-        print()
-        print("PLIK ZAKODOWANY:")
-        print(out)
-        print()
-        print("PLIK ZAKODOWANY (ze spacjami):")
-        print(out_separated)
-        print()
-
-        print("BITÓW POTRZEBNYCH DO ZAPISU:")
-        print('pięciobitowego | kodem zmiennej dł.')
-        print('-' * 35)
-        print('%-14r | %9s' % (len(string_fixed) * 5, len(out)))
-
-    with open(outfile, "w") as output:
-        output.write(out)
-    print("Zapisano wyjście do pliku \"" + outfile + "\".")
-    
-    with open(json_outfile, "w") as json_output:
-        huffman_sum = dict(huffman_code)
-        huffman_sum.update({v: k for k, v in huffman_code.items()})
-        json.dump(huffman_sum, json_output, indent = 2)
-    print("Zapisano słownik do pliku \"" + json_outfile + "\".")
+    action = int(input("Wybierz działanie: "))
+    print("1. Zakoduj")
+    print("2. Dekoduj")
+    print("3. Wyjdź")
+    match action:
+        case 1:
+            encode(infile, verbose, outfile, json)
+        case 2:
+            decode(infile, json)
+        case 3:
+            quit()
+        case _:
+            print("Nie rozpoznano działania")
 
 if __name__ == "__main__":
     main()
